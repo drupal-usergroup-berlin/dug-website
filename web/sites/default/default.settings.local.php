1e07971882781fdf7abad3a9bc0eb927a02ddef4
Mark Engelhardt <mark@yeahind.com>
2016-03-19 16:49:13 +0100

    adds template for local settings

---
+++ web/sites/default/default.settings.local.php
@@ -0,0 +1,14 @@
+<?php
+
+$databases['default']['default'] = array (
+ 'database' => '',
+  'username' => '',
+  'password' => '',
+  'host' => 'localhost',
+  'port' => '',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+);
+
+$settings['hash_salt'] = 'whatever';
