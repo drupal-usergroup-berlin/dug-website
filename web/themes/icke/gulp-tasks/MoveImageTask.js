d0a67b1520b0e050b65e3b398b9d6fd98aace0ab
Fabian Iwand <ciss@1632364.no-reply.drupal.org>
2016-03-20 14:16:23 +0100

    simplify gulp tasks

--- web/themes/icke/gulp-tasks/MoveImageTask.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('move_images', function () {
-  gulp.src('./bower_components/leaflet/dist/images/*')
-    .pipe(gulp.dest('../docroot/images'));
-
-  return gulp.src('./images/*')
-    .pipe(gulp.dest('../docroot/images'));
-});
