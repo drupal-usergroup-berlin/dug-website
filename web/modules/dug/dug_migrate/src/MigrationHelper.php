80147e0c9c8364660193ae2b6ff3aaa2ecb827d4
Florian Weber <florian.weber@ueberbit.de>
2016-03-20 17:07:43 +0100

    Migrate field_location_reference to field_location

--- web/modules/dug/dug_migrate/src/MigrationHelper.php
+++ web/modules/dug/dug_migrate/src/MigrationHelper.php
@@ -18,4 +18,43 @@ class MigrationHelper {
     return _filter_url($text, $filter);
   }
 
+  public static function locationToAddress($nid) {
+    $map = [
+      1 => [
+        'country_code' => 'DE',
+        'organization' => 'LOOM',
+        'locality' => 'Berlin',
+        'postal_code' => '10623',
+        'address_line1' => 'Goethestraße 2-3'
+      ],
+      16 => [
+        'country_code' => 'DE',
+        'organization' => 'RedSight',
+        'locality' => 'Berlin',
+        'postal_code' => '10587',
+        'address_line1' => 'Eosanderstraße 21e'
+      ],
+      133 => [
+        'country_code' => 'DE',
+        'organization' => 'betahaus',
+        'locality' => 'Berlin',
+        'postal_code' => '10969',
+        'address_line1' => 'Prinzessinnenstraße 19'
+      ],
+      642 => [
+        'country_code' => 'DE',
+        'organization' => 'Reinblau',
+        'locality' => 'Berlin',
+        'postal_code' => '13359',
+        'address_line1' => 'Osloer Straße 17'
+      ]
+    ];
+
+    if (isset($map[$nid])) {
+      return $map[$nid];
+    }
+
+    return NULL;
+  }
+
 }
